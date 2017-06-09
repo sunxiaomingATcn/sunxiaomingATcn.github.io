b5f3138d25ff934cf80a2bd72e9249194f9b70bf
xiaomingSun <767694331@qq.com>
2017-06-09 09:06:18 +0800

    update

---
+++ SXM_DEMO/requireJS/main.js
@@ -0,0 +1,14 @@
+require.config({
+　　　　paths: {
+　　　　　　"jquery": "jquery.min",
+　　　　　　"underscore": "underscore.min",
+　　　　　　"backbone": "backbone.min",
+		   "aa":"a",
+		   "bb":"b"
+　　　　}
+　　});
+
+
+require(['aa','bb'],function(){
+	
+})
