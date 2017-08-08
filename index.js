ef7cbe24aa22aa661bdd4b7b9320297ac30b9415
xiaomingSun <767694331@qq.com>
2017-08-08 15:12:04 +0800

    更新个人主页

--- index.js
+++ index.js
@@ -1,101 +1,228 @@
-
-
 $.fn.extend({
-	typing:function(str,timeInterval){
+	typing: function(str, timeInterval) {
 		var _this = $(this)
 		timeInterval = timeInterval || 200
 		var len = str.length
 		var thisWordIndex = 0
-		if( typeof str == 'string' && len > 0 ){
-			var timer = setInterval(function(){
-				_this.append('<span>'+ str[thisWordIndex] +'</span>')
+		if(typeof str == 'string' && len > 0) {
+			var timer = setInterval(function() {
+				_this.append('<span>' + str[thisWordIndex] + '</span>')
 				thisWordIndex++;
-				if(thisWordIndex == len ){
+				if(thisWordIndex == len) {
 					clearInterval(timer)
 				}
-			},timeInterval)
-			
-			
+			}, timeInterval)
+
 		}
 	},
 	//滚动到顶部
-	scrollAppoint:function(timeInterval){
+	scrollAppoint: function(timeInterval) {
 		timeInterval = timeInterval || 500
-		var scrollTop  = $(this).offset().top
-		$('body').animate({'scrollTop':scrollTop},timeInterval)
+		var scrollTop = $(this).offset().top
+		$('body').animate({
+			'scrollTop': scrollTop
+		}, timeInterval)
 	},
 	//鼠标跟随
-	followMouse:function(MouseElement,timeInterval){
-		timeInterval = timeInterval || 500
+	followMouse: function(MouseElement, timeInterval) {
+		timeInterval = timeInterval || 500;
 		var w = $(this).width();
 		var h = $(this).height();
-		$(this).bind("mouseenter mouseleave",function(e){
+		$(this).bind("mouseenter mouseleave", function(e) {
 			var w = $(this).width();
 			var h = $(this).height();
 			var x = (e.pageX - $(this).offset().left - (w / 2)) * (w > h ? (h / w) : 1);
 			var y = (e.pageY - $(this).offset().top - (h / 2)) * (h > w ? (w / h) : 1);
 			var direction = Math.round((((Math.atan2(y, x) * (180 / Math.PI)) + 180) / 90) + 3) % 4;
-			var eventType = e.type;
-			var dirName = new Array('上方','右侧','下方','左侧');
-			if(e.type == 'mouseenter'){
-				switch( direction ){
-					case 0://上方
-						$(this).find(MouseElement).css({'top':-h,left:0});
-					break;
-					case 1://右侧
-						$(this).find(MouseElement).css({'top':0,left:w});
-					break;
-					case 2://下方
-						$(this).find(MouseElement).css({'top':h,left:0});
-					break;
-					case 3://左侧
-						$(this).find(MouseElement).css({'top':0,left:-w});
-					break;
-				}
-				$(this).find(MouseElement).animate({'top':0,left:0},timeInterval)
-			}else{
-				switch( direction ){
-					case 0://上方
-						$(this).find(MouseElement).animate({'top':-h,left:0},timeInterval);
-					break;
-					case 1://右侧
-						$(this).find(MouseElement).animate({'top':0,left:w},timeInterval);
-					break;
-					case 2://下方
-						$(this).find(MouseElement).animate({'top':h,left:0},timeInterval);
-					break;
-					case 3://左侧
-						$(this).find(MouseElement).animate({'top':0,left:-w},timeInterval);
-					break;
-				}
-				
-			}
-			
-		})
 		
+			if(e.type == 'mouseenter') {
+				$(this).find('img').css({
+					'transform': 'scale(1.1,1.1)',
+					'transition': '1s all'
+				});
+				switch(direction) {
+					case 0: //上方
+						$(this).find(MouseElement).css({
+							'top': -h,
+							left: 0
+						});
+						break;
+					case 1: //右侧
+						$(this).find(MouseElement).css({
+							'top': 0,
+							left: w
+						});
+						break;
+					case 2: //下方
+						$(this).find(MouseElement).css({
+							'top': h,
+							left: 0
+						});
+						break;
+					case 3: //左侧
+						$(this).find(MouseElement).css({
+							'top': 0,
+							left: -w
+						});
+						break;
+				}
+				$(this).find(MouseElement).animate({
+					'top': 0,
+					left: 0
+				}, timeInterval)
+			} else {
+				$(this).find('img').css({
+					'transform': 'scale(1,1)',
+					'transition': '1s all'
+				});
+				switch(direction) {
+					case 0: //上方
+						$(this).find(MouseElement).animate({
+							'top': -h,
+							left: 0
+						}, timeInterval);
+						break;
+					case 1: //右侧
+						$(this).find(MouseElement).animate({
+							'top': 0,
+							left: w
+						}, timeInterval);
+						break;
+					case 2: //下方
+						$(this).find(MouseElement).animate({
+							'top': h,
+							left: 0
+						}, timeInterval);
+						break;
+					case 3: //左侧
+						$(this).find(MouseElement).animate({
+							'top': 0,
+							left: -w
+						}, timeInterval);
+						break;
+				}
+
+			}
+
+		})
+
 	}
 })
 
-$(function(){
+$(function() {
 	//打印效果
-	$('.web-developer-describe').typing('Talk is cheap , show me the code',100);
-	
-	
-	$('.myWorksTitle').click(function(){
+	$('.web-developer-describe').typing('Talk is cheap , show me the code', 100);
+
+	$('.myWorksTitle').click(function() {
 		$('.my-achievements').scrollAppoint()
 	})
 	//鼠标跟随效果
 	$('.one-work').followMouse('.work-inside-des')
-	
-	
-	$('.call-me').click(function(){
+
+	//显示邮箱
+	$('.call-me').click(function() {
 		$('.my-emial').addClass('animate-show-up')
 	})
-	$(window).on('scroll touchend',function(){
+	//联系我动画
+	$(window).on('scroll touchend', function() {
 
-		var onPage3 = $('.pageContactMe').offset().top-$(window).scrollTop();
-		onPage3 <= 5 ? $('.pageContactMe').addClass('animate-show').removeClass('animate-show-callback'):$('.pageContactMe').addClass('animate-show-callback').removeClass('animate-show')
-		
-		
+		var onPage3 = $('.pageContactMe').offset().top - $(window).scrollTop();
+		onPage3 <= 5 ? $('.pageContactMe').addClass('animate-show').removeClass('animate-show-callback') : $('.pageContactMe').addClass('animate-show-callback').removeClass('animate-show')
+
 	})
+	
+	
+	//canvas heart
+	var canvas = document.getElementById("canvasHeart");
+	var ctx = canvas.getContext("2d");
+
+	var ww, wh;
+	ctx.strokeStyle = "red";
+	ctx.shadowBlur = 25;
+	ctx.shadowColor = "hsla(0, 100%, 60%,0.7)";
+	
+	var precision = 100;
+	var hearts = [];
+	var mouseMoved = false;
+	onResize();
+	//window.addEventListener("mousemove", onMove);
+	//window.addEventListener("touchmove", onMove);
+	//window.addEventListener("resize", onResize);
+	requestAnimationFrame(render);
+
+	function onResize() {
+		ww = canvas.width = window.innerWidth;
+		wh = canvas.height = window.innerHeight;
+	}
+	
+	function onMove(e) {
+		mouseMoved = true;
+		if(e.type === "touchmove") {
+			hearts.push(new Heart(e.touches[0].clientX, e.touches[0].clientY));
+			hearts.push(new Heart(e.touches[0].clientX, e.touches[0].clientY));
+		} else {
+			hearts.push(new Heart(e.clientX, e.clientY));
+			hearts.push(new Heart(e.clientX, e.clientY));
+		}
+	}
+	
+	var Heart = function(x, y) {
+		this.x = x || Math.random() * ww;
+		this.y = y || Math.random() * wh;
+		this.size = Math.random() + 1;
+		this.shadowBlur = Math.random() * 10;
+		this.speedX = (Math.random() - 0.5 ) * 8;
+		this.speedY = (Math.random() - 0.5 ) * 8;
+		this.speedSize = Math.random() * 0.05 + 0.01;
+		this.opacity = 1;
+		this.vertices = [];
+		for(var i = 0; i < precision; i++) {
+			var step = (i / precision - 0.5) * (Math.PI * 2);
+			var vector = {
+				x: (15 * Math.pow(Math.sin(step), 3)),
+				y: -(13 * Math.cos(step) - 5 * Math.cos(2 * step) - 2 * Math.cos(3 * step) - Math.cos(4 * step))
+			}
+			this.vertices.push(vector);
+		}
+	}
+	
+	Heart.prototype.draw = function() {
+		this.size -= this.speedSize;
+		this.x += this.speedX;
+		this.y += this.speedY;
+
+		ctx.save();
+		ctx.translate(-1000, this.y);
+		ctx.scale(this.size, this.size);
+		ctx.beginPath();
+		
+		for(var i = 0; i < precision; i++) {
+			var vector = this.vertices[i];
+			ctx.lineTo(vector.x, vector.y);
+		}
+		ctx.globalAlpha = this.size;
+		ctx.shadowBlur = Math.round((3 - this.size) * 10);
+		ctx.shadowColor = "hsla(0, 100%, 60%,0.7)";
+		ctx.shadowOffsetX = this.x + 1000;
+		ctx.globalCompositeOperation = "screen"
+		ctx.closePath();
+		ctx.fill();
+		ctx.restore();
+	};
+	
+	function render(a) {
+		requestAnimationFrame(render);
+	
+		hearts.push(new Heart())
+		ctx.clearRect(0, 0, ww, wh);
+		for(var i = 0; i < hearts.length; i++) {
+			hearts[i].draw();
+			if(hearts[i].size <= 0) {
+				hearts.splice(i, 1);
+				i--;
+			}
+		}
+	}
+	
+	
 })
